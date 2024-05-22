a74d3ee912f22a6cdcaa1c4238435a193cf9164a
Abd El Ouadoud KHOURI <128540597+AbdElOuadoudK@users.noreply.github.com>
2024-05-23 00:21:41 +0100

    Update __init__.py

--- source_code/ClientSide/Scripts/cltpack/__init__.py
+++ source_code/ClientSide/Scripts/cltpack/__init__.py
@@ -1,34 +1,29 @@
-"""
-Initialization module for the client package.
-
-This module imports key components and sets up initial configurations such as warnings and logging.
-
-By @Ouadoud
-"""
-
 from .dataformat import Dataset, load_train_data
 from .neuralnet import NeuralNet
 from .train import _main, test
 from .client import Client, Agent_Algorithm
-from .globals import LOAD_TRAIN_DATA_KWARGS, DEVICE, MODEL
+from .globals import (LOAD_TRAIN_DATA_KWARGS, DEVICE, MODEL, DATA_PATH, LOGS_PATH)
 import warnings
 import logging
+from flwr.common.logger import configure
+from os import path
 
-# Suppress FutureWarnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
-
-# Disable logging for INFO level and below
+configure(identifier="FLProjectExperiment", filename=path.join(LOGS_PATH, "client_logs.txt"))
+warnings.simplefilter(action='ignore', category=FutureWarning) # ,DeprecationWarning
+warnings.simplefilter(action='ignore', category=DeprecationWarning)
 logging.disable(logging.INFO)
 
-__all__ = [
-    "Dataset",
-    "load_train_data",
-    "NeuralNet",
-    "_main",
-    "test",
-    "Client",
-    "Agent_Algorithm",
-    "LOAD_TRAIN_DATA_KWARGS",
-    "DEVICE",
-    "MODEL"
-]
+
+__all__ = ["Dataset",
+           "load_train_data",
+           "NeuralNet",
+           "_main",
+           "test",
+           "Client",
+           "Agent_Algorithm",
+           "LOAD_TRAIN_DATA_KWARGS",
+           "DEVICE",
+           "MODEL",
+           "DATA_PATH",
+           "LOGS_PATH"
+          ]
