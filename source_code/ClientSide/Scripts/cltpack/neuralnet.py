349283a540da00131c84dde3c21c1cbfc6dc32b8
Abd El Ouadoud KHOURI <128540597+AbdElOuadoudK@users.noreply.github.com>
2024-05-23 00:23:56 +0100

    Update neuralnet.py

--- source_code/ClientSide/Scripts/cltpack/neuralnet.py
+++ source_code/ClientSide/Scripts/cltpack/neuralnet.py
@@ -1,29 +1,16 @@
 from collections import OrderedDict
-from typing import List
-import numpy as np
+from typing import List, Optional
+import numpy
 import torch
 import torch.nn as nn
 
 class NeuralNet(nn.Module):
     """
     A simple neural network model for classification.
-
-    By @Ouadoud
     """
-
     def __init__(self):
         """
         Initialize the neural network model.
-        
-        Layers:
-            - Linear: Fully connected layer with 122 input features and 64 output features.
-            - ReLU: Activation function.
-            - Linear: Fully connected layer with 64 input features and 32 output features.
-            - ReLU: Activation function.
-            - Linear: Fully connected layer with 32 input features and 16 output features.
-            - ReLU: Activation function.
-            - Linear: Fully connected layer with 16 input features and 5 output features.
-            - Dropout: Regularization layer with 0.5 dropout probability.
         """
         super(NeuralNet, self).__init__()
         self.dropout = nn.Dropout(p=0.5)
@@ -35,7 +22,7 @@ class NeuralNet(nn.Module):
             nn.Linear(32, 16),
             nn.ReLU(),
             nn.Linear(16, 5)
-        )
+        )        
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -49,22 +36,10 @@ class NeuralNet(nn.Module):
         """
         return self.sequential(self.dropout(x))
 
-    def get_parameters(self) -> List[np.ndarray]:
-        """
-        Get the parameters of the model.
-        
-        Returns:
-            List[np.ndarray]: List of model parameters as NumPy arrays.
-        """
+    def get_parameters(self,) -> List[numpy.ndarray]:
         return [val.cpu().numpy() for _, val in self.state_dict().items()]
-
-    def set_parameters(self, parameters: List[np.ndarray]) -> None:
-        """
-        Set the parameters of the model.
-        
-        Args:
-            parameters (List[np.ndarray]): List of model parameters as NumPy arrays.
-        """
+    
+    def set_parameters(self, parameters: List[numpy.ndarray]) -> None:
         params_dict = zip(self.state_dict().keys(), parameters)
         state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
         self.load_state_dict(state_dict, strict=True)
