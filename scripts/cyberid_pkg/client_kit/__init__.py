57e8b3e9ee4366b164f9f08615b96ffedd6d66de
Abd El Ouadoud KHOURI <128540597+AbdElOuadoudK@users.noreply.github.com>
2024-10-16 20:14:04 +0100

    refactor(client_kit): reformulate code section

--- scripts/cyberid_pkg/client_kit/__init__.py
+++ scripts/cyberid_pkg/client_kit/__init__.py
@@ -1,8 +1,12 @@
-
 from .client_utils import generate_client_fn
 from .client import TrainingAgent
+
+from ..globals import LOGS_PATH, DATA_PATH
+from logging import Formatter, FileHandler, DEBUG
+from os.path import join
 
 __all__ = [
     "generate_client_fn",
     "TrainingAgent",
+    "create_file_handler",
 ]
