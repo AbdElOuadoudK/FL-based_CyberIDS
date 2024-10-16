364a8821ebeb9251c8101942c37886d08717523c
Abd El Ouadoud KHOURI <128540597+AbdElOuadoudK@users.noreply.github.com>
2024-10-16 20:08:00 +0100

    refactor(client_utils.py): update logging utilities and refactor client generator
    This commit refactors the logging setup and introduces several key changes to the `client_utils.py` module:
    
    ### Changes:
    - **Imports:**
      - Imported logging utilities, including the predefined `flwr` logger instance for consistent logging.
      - Introduced the constant `BATCH_SIZE` from the `globals.py` module to standardize batch size handling.
    
    - **Logging Logic:**
      - Refactored the logging logic by moving it outside the client generator function to create a global and unified logging instance.
      - This ensures consistent logging behavior across different clients launched, adhering to a general and expected pattern.
    
    ### Why this is important:
    The updates streamline logging for better consistency and management across multiple client instances. By refactoring the logging setup, we ensure that logs are captured in a unified manner across the project, simplifying debugging and tracking.

--- scripts/cyberid_pkg/client_kit/client_utils.py
+++ scripts/cyberid_pkg/client_kit/client_utils.py
@@ -1,23 +1,27 @@
-
-from ..globals import LOGS_PATH, DATA_PATH
+import os
+from ..globals import LOGS_PATH, DATA_PATH, BATCH_SIZE
 from ..dataformat import load_train_data
 from .client import TrainingAgent
-from flwr.common.logger import configure
+from logging import Formatter, FileHandler, DEBUG, getLogger
+from flwr.common.logger import logger
+#from ray import logger as ray_logger
 from os.path import join
 from pandas import read_feather
 
-def generate_client_fn(distribution: dict = None, epochs: int = 1):
+ray_logger = getLogger("ray." + "client_1")
+ray_logger.setLevel(DEBUG)
+file_handler = FileHandler(filename=join(LOGS_PATH, f'client_{1}.log'))
+formatter = Formatter(f'%(name)s | %(filename)s | %(asctime)s | %(thread)d (client_{1})| %(levelname)s | %(message)s')
+file_handler.setFormatter(formatter)
+ray_logger.addHandler(file_handler)
+
+def generate_client_fn(train_data, distribution: dict = None, epochs: int = 1):
 
     def client_fn(cid:str):
-        
-        configure(identifier="IDS_Learning_Logs", filename=join(LOGS_PATH, f"client_{int(cid)}_logs.txt"))
-        
-        data_path = join(DATA_PATH, "train.feather")
-        train_data = read_feather(data_path)
         train_loader, valid_loader = load_train_data(data = (train_data.drop(['intrusion'], axis=1), train_data[['intrusion']]),
-                                                     size = 1/100, ### add int(cid) generation logic...
+                                                     size = 1/10000, ### add int(cid) generation logic...
                                                      valid_rate = .45,
-                                                     batch_size = 512,
+                                                     batch_size = BATCH_SIZE,
                                                      distribution = distribution,
                                                      sparse_y = False
                                                     )
